e5b7e1289b8893ac7b14741163e87971a7ad0ba6
Ali Bokhari <abokhari44@gmail.com>
2018-07-31 11:15:39 -0400

    Adding new role for ComputeHCI

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
-openstack overcloud deploy --templates --timeout 90 \
+time openstack overcloud deploy \
+  --templates --timeout 90 \
+  -r /home/stack/templates/custom_roles_data.yaml \
   -e /home/stack/templates/node-info.yaml\
   -e /home/stack/templates/overcloud_images.yaml \
   -e /home/stack/templates/environments/network-isolation.yaml \
   -e /home/stack/templates/environments/network-environment.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+  -e /home/stack/templates/environments/ceph-custom-config.yaml \
   --ntp-server pool.ntp.org \
   | tee overcloud-install.log
+
